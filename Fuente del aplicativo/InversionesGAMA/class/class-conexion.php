f0ad60c77ce1e25d3b2e812859669a2e17aa5f4a
Crisjoleo <crisjoleo.durn@hotmail.com>
2020-08-08 16:51:16 -0600

    Proveedor
    Agragada clase para conexio a base de datos oracle, agregada clase proveedor, y boton de  "Guardar "en la pesta;a de proveedores inserta los datos en la base

---
+++ Fuente del aplicativo/InversionesGAMA/class/class-conexion.php
@@ -0,0 +1,46 @@
+<?php
+	
+	class Conexion{
+		private $hostDB = "localhost/XE";
+		private $usuarioDB = "GAMA";
+		private $passDB = "legolas";
+		private $link;
+
+		public function __construct(){
+			$this->link = oci_connect("GAMA","legolas","localhost/XE");
+			if (!$this->link) {
+	    	$e = oci_error();
+	    	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	       }
+		}
+	
+
+	public function ejecutarConsulta($sql){
+			$query = oci_parse($this->link,$sql);
+			 oci_execute($query);
+            return $query;
+		}
+
+
+		public function obtenerFila($resultado){
+			return oci_fetch_array($resultado,OCI_BOTH);
+		}
+
+
+		 public function cerrarConexion(){
+			oci_close($this->link);
+		}
+
+
+		public function antiInyeccion($texto){
+		//[INDENT] return str_replace("'", "\'", $texto); [/INDENT] 
+
+		}
+
+		public function cantidadRegistros($resultado){
+			return oci_num_rows($resultado);
+			
+		}
+
+    }
+?>
